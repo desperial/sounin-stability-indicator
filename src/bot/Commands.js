3949b2db5a9ac9b2bd3d6e0bd8b1c91e699ca892
Desperial <premodialburst@gmail.com>
2018-08-30 12:35:54 +0500

    trello requests added. Also, tts, new commands etc etc.

--- src/bot/Commands.js
+++ src/bot/Commands.js
@@ -2,14 +2,16 @@ const Discord = require('discord.js');
 const sosi = new Discord.Client();
 const config = require('../config.json');
 const lines = require('../lines.json');
-const ytdl = require('ytdl-core');
 const fs = require("fs");
-const Statistic = require('../bot/Statistic');
-const Model = require('../models');
 const snekfetch = require('snekfetch');
+const lastLeave = require('../last-leave.json');
+const Trello = require("node-trello");
+
+const trelloController = new Trello(config.trelloApi, config.trelloToken);
 
 var commands = {
   "help": {
+    descriptionShort: "Инфо о команде",
     description: "Список команд, их описание и методы использования.",
     params:{
       "cmd":{
@@ -18,7 +20,25 @@ var commands = {
       },
     },
     process: (bot, msg, souninId, params) => {
-      var helpText = `##SoSI-HUI: Sounin Stability Indicator - Helping User Interface\n\n`;
+      
+      var embedData = {
+        author: {
+          name: bot.user.username,
+          icon_url: bot.user.avatarURL
+        },
+        color: 0x00FFFF,
+        title: "SoSI-HUI: Sounin Stability Indicator - Helping User Interface",
+        url: 'https://github.com/desperial/sounin-stability-indicator',
+        footer:{
+          text: "Можете звездануть бота на GitHub",
+          icon_url: bot.user.avatarURL
+        }
+      };
+      var embed = new Discord.RichEmbed(embedData)
+
+      embed.setThumbnail("https://image.ibb.co/c7LU7p/14483994260450.png")
+      var helpText ='';
+
       if(params.length>0){
         params.forEach((val, ind, arr)=>{
           var commandHelp = Object.keys(commands).find((val2find)=>{
@@ -26,56 +46,62 @@ var commands = {
           });
 
           if(commandHelp){
+            commandHelp==='huisya'?embed.setColor(0xFF0000):null;
             var paramsHelp ='';
             if(commands[commandHelp].params){
               Object.keys(commands[commandHelp].params).forEach((valP,indP,arrP) => {
                 paramsHelp += commands[commandHelp].params[valP].required===true?`<${valP}> `:`[${valP}] `;
               })
             }
-            var cmdString = `\n${commandHelp} ${paramsHelp}\n`;
-            var markdownEqalSign = '=';
-            helpText += cmdString;
-            helpText += markdownEqalSign.repeat(cmdString.length-2) + '\n';
-            helpText += commands[commandHelp].description + '\n\n';
+            embed.addField(`${commandHelp} ${paramsHelp}`, commands[commandHelp].description )
+            embed.addBlankField(true)
+
 
             if(commands[commandHelp].params){
-              paramsHelp = '';
-              helpText += 'Параметры:\n'
+              var paramsHelp ='';
+
               Object.keys(commands[commandHelp].params).forEach((valP,indP,arrP) => {
-                paramsHelp += `[${indP+1}]: ${valP}: ${commands[val].params[valP].required===true?`(обязательный)`:`(опциональный)`} ${commands[val].params[valP].description}\n`
+                paramsHelp += `**${valP}**: ${commands[val].params[valP].required===true?`(обязательный)`:`(опциональный)`} ${commands[val].params[valP].description}\n`
               })
+
+              embed.addField(`Параметры`, paramsHelp )
+              embed.addBlankField(true)
             }
 
-            helpText += paramsHelp;
-
             if(commands[commandHelp].usage){
-              helpText += '\nПример использования:\n\n'
-              helpText += commands[commandHelp].usage
+              embed.addField(`Пример использования:`, commands[commandHelp].usage )
             }
             
           }
         })
 
-        helpText += "Можете звездануть бота на GitHub: https://github.com/desperial/sounin-stability-indicator";
-        return msg.channel.send(helpText, {code: 'markdown'})
+        msg.author.send(embed);
+        return msg.reply('информация о командах отправлена Вам личным сообщением.')
       }else{
+        embed.setDescription(`Ниже приведен ~~почти~~ полный список команд бота.\n Чтобы подробнее почитать об использовании команды, введите ${config.prefix}help [cmd]\nНапример, ${config.prefix}help prefix расскажет Вам, как менять префикс бота.`)
+        var commandsList = '';
+        var descriptionList = '';
         Object.keys(commands).forEach((val,ind,array) => {
-          var paramsHelp = '';
-          if(commands[val].params){
-            Object.keys(commands[val].params).forEach((valP,indP,arrP) => {
-              paramsHelp += commands[val].params[valP].required===true?`<${valP}> `:`[${valP}] `;
-            })
+          if(commands[val].hidden === undefined){
+            var paramsHelp = '';
+            if(commands[val].params){
+              Object.keys(commands[val].params).forEach((valP,indP,arrP) => {
+                paramsHelp += commands[val].params[valP].required===true?`<${valP}> `:`[${valP}] `;
+              })
+            }
+            commandsList += `\`${config.prefix}${val} ${paramsHelp} - ${commands[val].descriptionShort}\`\n\n`
+            descriptionList += `\n`
           }
-          helpText += `[${ind+1}]: ${config.prefix}${val} ${paramsHelp}\n`
-          helpText += `${commands[val].description}\n\n`;
         });
-        helpText += "Можете звездануть бота на GitHub: https://github.com/desperial/sounin-stability-indicator";
-        return msg.channel.send(helpText, {code: 'markdown'})
+        embed.addField(`Командs`, commandsList, true)
+        msg.author.send(embed);
+        return msg.reply('полный список команд отправлен Вам в личные сообщения.')
       }
     }
   },
 
   "prefix":{
+    descriptionShort: "смена префикса",   
     description: "Меняет префикс бота.",
     params:{
       "new prefix":{
@@ -94,11 +120,12 @@ var commands = {
   
   "check-last-leave":{
     description:`Выводит последний таймстамп лива Соунина по часовому поясу +5 GMT и количество дней, которые он продержался. Почему, скажем, не по МСК?
-     \"Это потому что в жоже не было станда про часовые пояса или про Гринвич\" (С) Pickleman.`,
+     \"*Это потому что в жоже не было станда про часовые пояса или про Гринвич*\" (С) Pickleman.`,
     process: function(bot,msg,souninId, params=[]){
+
       const sounin = msg.guild.members.get(souninId);
       if (sounin) {
-        var souninLeft = sounin.joinedTimestamp;
+        var souninLeft = lastLeave.end;
         var souninLeftDate = new Date(souninLeft);
         var dd = souninLeftDate.getDate(); if (dd<10) dd='0'+dd;
         var mn = souninLeftDate.getMonth() + 1; if (mn<10) mn='0'+mn;
@@ -117,31 +144,35 @@ var commands = {
     }
   },
 
-  "statistic":{
-    description: "Статистика по ливам Соунина",
-    process: function(bot,msg,souninId, params){
-      var statisticModel = Model('Statistic');
+  // "statistic":{
+  //   descriptionShort: "статистика по ливам Соунина",
+
+  //   description: "Статистика по ливам Соунина",
+  //   process: function(bot,msg,souninId, params){
+  //     var statisticModel = Model('Statistic');
      
-      statisticModel.findOne({}, (err,doc) => {
-        const sounin = msg.guild.members.get(souninId);
-        if (sounin) {
-          var souninLeft = sounin.joinedTimestamp;
-          var daysSinceSouninLeft = Math.floor((Date.now() - souninLeft) / (1000 * 60 * 60 * 24));
+  //     statisticModel.findOne({}, (err,doc) => {
+  //       const sounin = msg.guild.members.get(souninId);
+  //       if (sounin) {
+  //         var souninLeft = sounin.joinedTimestamp;
+  //         var daysSinceSouninLeft = Math.floor((Date.now() - souninLeft) / (1000 * 60 * 60 * 24));
   
-        }
-        msg.reply(`за время моей официальной работы было зафиксировано ${doc.leavesCount} ливов\r\n\
-            Рекордный срок нахождения Соунина на сервере из зафиксированных:\r\n\
-              ${Math.floor(doc.maxStabilityDurationWeeks)} недель \r\n\
-              ... это ${Math.floor(doc.maxStabilityDurationDays)} дней \r\n\
-              ... это ${Math.floor(doc.maxStabilityDurationHours)} часов \r\n\
-            Поскольку Соунин ещё не ливнул, его нынешний результат в ${daysSinceSouninLeft} дней ${(daysSinceSouninLeft>doc.maxStabilityDurationDays)?'(идет на рекорд, кстати!)':''} в учет не взят
-        `);
-      });
+  //       }
+  //       msg.reply(`за время моей официальной работы было зафиксировано ${doc.leavesCount} ливов\r\n\
+  //           Рекордный срок нахождения Соунина на сервере из зафиксированных:\r\n\
+  //             ${Math.floor(doc.maxStabilityDurationWeeks)} недель \r\n\
+  //             ... это ${Math.floor(doc.maxStabilityDurationDays)} дней \r\n\
+  //             ... это ${Math.floor(doc.maxStabilityDurationHours)} часов \r\n\
+  //           Поскольку Соунин ещё не ливнул, его нынешний результат в ${daysSinceSouninLeft} дней ${(daysSinceSouninLeft>doc.maxStabilityDurationDays)?'(идет на рекорд, кстати!)':''} в учет не взят
+  //       `);
+  //     });
       
-    }
-  },
+  //   }
+  // },
 
   "kisya":{
+    descriptionShort: "случайная кися",
+
     description: `Хочешь кисю?
      人____人
     ≧(◕ ‿‿ ◕)≦`,
@@ -153,6 +184,8 @@ var commands = {
   },
 
   "owl":{
+    descriptionShort: "случайная совушка",
+
     description: `А совушку хочешь?
   ,     ,
   )\\___/(
@@ -164,7 +197,9 @@ var commands = {
     }
   },
 
-  "pickrandom":{
+  "picrandom":{
+    descriptionShort: "случайная пикча из GIPHY",
+
     description: `Хочешь чего-то ещё (например, пиздюлей) а отдельной команды нет?\n Вот она!`,
     params:{
       "tag":{
@@ -172,6 +207,7 @@ var commands = {
         required:true
       },
     },
+    usage:`${config.prefix}picrandom owl выведет картинку по тегу "сова". Никакой гарантии, что это действительно будет сова. Всё зависит от тегов непосредственно в GIPHY`,
     process: async (bot,msg,souninId, params)=>{
       if(params.length<1){
         msg.reply(lineRandom(lines.giphyNoTags))
@@ -190,25 +226,179 @@ var commands = {
   },
   
   "kazino":{
-    description:"Тест яндекс.спичкита",
+    descriptionShort: "выразить недовольство этим казино в войсах",
+    description:"Тест яндекс.спичкита. Бот заходит в войс-канал, декларирует его, как казино, упоминает об оральном сношении с ним и скоропостижно покидает заведение.",
     process: async (bot,msg,souninId, params)=>{
       var channel = msg.member.voiceChannel
       const connection = await channel.join();
-      var url = ySpeechRequest('Ебаный в рот этого казино');
-      console.log(url);
-      var dispatcher = connection.playArbitraryInput(url, )
-      console.log(connection);
+      var url = ySpeechRequest('Ёбаный рот этого казино, блять! Ты кто такой, сука? Чтоб это сделать? Вы чё, дебилы? Вы чё, ебанутые что ли, блять? Вот действительно, вы внатуре ебанутые? Этот сидит, чешет колоду, этот sтоит говорит, "я тебе сейчас тоже раздам", блять!', 'evil');
+      var streamOptions = {
+        volume:2,
+        passes:5,
+        bitrate:64
+      }
+      var dispatcher = connection.playArbitraryInput(url, streamOptions)
+      dispatcher.on('speaking', (value)=>{
+        console.log(value);
+      })
+
+      dispatcher.on('end', (reason)=>{
+        channel.leave();
+      })
+      
+    }
+  },
+
+  "tts":{
+    descriptionShort: "Сказануть фразу в войс-чате",
+    description:"Бот заходит в войс-канал, в котором Вы находитесь во время исполнения команды, говорит реквестированную фразу и покидает канал.",
+    params:{
+      "text":{
+        description:"Текст, который скажет бот прямо в войс-чат.",
+        required:true
+      },
+      "-g|-n|-e":{
+        description:"флаг эмоционального окраса чтения TTS. **g**ood, **n**eutral, **e**vil. По-умолчанию -n",
+        required:false
+      }
+    },
+    process: async (bot,msg,souninId, params)=>{
+      
+      if(params.length>0){
+        const emotionFlag = params.find((val,ind,arr) => { 
+          if(val === '-g' || val === '-e' || val === '-n'){
+            params.splice(ind, 1);
+            return val;
+          }
+        })
+
+        var emotion = 'neutral'
+        if(emotionFlag){
+          switch (emotionFlag){
+
+            case '-g': 
+              emotion = 'good';
+              break
+            
+            case '-n': 
+              emotion = 'neutral';
+              break
+
+            case '-e': 
+              emotion = 'evil';
+              break
+
+          }
+        }
+        var textToSpeech
+        params.length>0
+          ?textToSpeech = params.join(' ')
+          :msg.reply('Текста не осталось. Вы что же, ввели только флаг?');
+
+        var channel = msg.member.voiceChannel
+        if(!channel){
+          return msg.reply('Вы не находитесь в голосовом канале.')
+        }
+        const connection = await channel.join();
+        var url = ySpeechRequest(textToSpeech,emotion);
+        console.log(url)
+
+        var streamOptions = {
+          volume:2,
+          passes:5,
+          bitrate:64
+        }
+        var dispatcher = connection.playArbitraryInput(url, streamOptions)
+        dispatcher.on('speaking', (value)=>{
+        })
+  
+        dispatcher.on('end', (reason)=>{
+          channel.leave()
+        })
+
+      }else{
+        msg.reply('Текста совершенно нет!');
+      }
     }
   },
 
   "test":{
+    descriptionShort: "Пройди тест \"Кто ты из таверны <<Розовый Дилдоэзреаль?>>.\"",
+
     description: `Пройди тест "Кто ты из таверны <<Розовый Дилдоэзреаль?>>.`,
     process: async (bot,msg,souninId, params)=>{
       msg.reply("Обрабатываем результат...");
       var identifyer = msg.author.id
       msg.reply(lines[identifyer]);
     }
+  },
+
+  "huisya":{
+    description: `Данная команда внесена в реестр запрещенных команд SoSI-HUI.`,
+    usage: `Ты ныряешь в запретную магию, парень! Остановись, пока остановка не стала последней!`,
+    hidden: true,
+    process: async (bot,msg,souninId, params)=> {
+      msg.reply('HUISYA AWAKEN', {files:['https://dl.backbook.me/full/0efb3dead1.jpg']})
+    }
+  },
+
+  "request":{
+    description: "Создайте реквест желаемого функционала в трелло!",
+    descriptionShort: "Создать реквест в трелло",
+    params:{
+      "title":{
+        description: "заголовок задачи",
+        required: true
+      },
+      "description":{
+        description: "Описание задачи",
+        required: false
+      }
+    },
+    usage:`Это, пожалуй, первая команда, где аргументы надо прописывать в кавычках.\n${config.prefix}request "хочу шорткат функции Х" "добавь шорткат для функции Х, чтобы вместо ХХХ я вводил Х и экономил время и жизнь" - таким образом вы создадите новую карточку в Trello, которую я посмотрю С:`,
+    process: async (bot,msg,souninId, params)=> {
+
+      var fullParams;
+
+      params.length>0
+          ?fullParams = params.join(' ')
+          :msg.reply('Вы ничего не ввели.');
+
+      if(fullParams){
+        console.log(fullParams)
+
+        var paramsArray = fullParams.split('"')
+        console.log(paramsArray)
+
+        var title = paramsArray[1];
+        var description = `Отправлено: ${msg.author.username}\n`;
+        if (paramsArray[3] === undefined){
+         description += "Описание не оставлено, так что ориентируйся по сабжу."
+         
+        }else{
+          description += paramsArray[3]
+        }
+        var newCard =
+          {
+            name: title,
+            desc: description,
+            pos: "top",
+            due: null,
+            idList: config.trelloRequestList
+          };
+
+          trelloController.post('/1/cards', newCard, function (err, data) {
+            console.log(data);
+          });
+      }else{
+        return false;
+      }
+
+      
+    }
+
   }
+
 }
 
 function lineRandom(linesArray) {
@@ -221,8 +411,9 @@ async function giphyApi(query) {
   return await snekfetch.get(`http://api.giphy.com/v1/gifs/random?tag=${query}&api_key=${config.giphyKey}`);
 }
 
-function ySpeechRequest(text){
-  return encodeURI(`https://tts.voicetech.yandex.net/generate?key=${config.yaSpeechKit}&text=${text}&format=opus&lang=ru-RU&speaker=oksana&speed=1&emotion=evil`)
+function ySpeechRequest(text, emotion){
+  text += text.substring(0,7)
+  return encodeURI(`https://tts.voicetech.yandex.net/generate?key=${config.yaSpeechKit}&text=${text}&format=wav&quality=hi&lang=ru-RU&speaker=ermil&speed=1&emotion=${emotion}`)
 }
 
 module.exports = commands
--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ const sosi = new Discord.Client();
 const config = require('./config.json');
 const lines = require('./lines.json');
 const fs = require("fs");
+const evaNames = require("./eva_nicnames/last_nickchange.json");
+const evaChanges = require("./eva_nicnames/changes.json");
 const commands = require('./bot/Commands');
+const CronJob = require('cron').CronJob;
+
+var goodmorning = new CronJob('0 0 7 * * *', function() {
+    const johnyral = sosi.channels.get(config.channelId);
+    johnyral.send(`Доброе утро, чатик! :3`);
+}, null, true, 'Europe/Moscow');
 
 
 sosi.login(config.token);
 
 sosi.on('ready', () => {
   console.log('ready to serve!');
-
 });
 
 sosi.on('message', async (message) => {
@@ -18,6 +25,7 @@ sosi.on('message', async (message) => {
   var commandInput;
   var commandAction;
   var commandParams;
+  const sounin = await sosi.fetchUser(config.souninId);
   if (message.content.startsWith(config.prefix)){
     commandInput = message.content.split(config.prefix).slice(1)[0];
     commandAction = commandInput.split(" ").slice(0)[0];
@@ -36,6 +44,7 @@ sosi.on('message', async (message) => {
     try {
       commands[command].process(sosi,message,config.souninId,commandParams)
     }catch(err){
+        console.log(err);
         console.log('Command not fount');
         message.reply('Нет такой команды. Если вы хотите её добавить... Ну, увы.')
     }
@@ -44,6 +53,31 @@ sosi.on('message', async (message) => {
   //
   if (message.content === 'Спасибо, бот.') {
     message.reply(lineRandom(lines.urWelcome));
+  }
+
+  if (message.channel === sounin.dmChannel){
+    console.log(message.content)
+  }
+});
+
+sosi.on('guildMemberUpdate', (oldMember, newMember) => {
+  if ((oldMember.id === config.evaId) && (oldMember.nickname != newMember.nickname)){
+    var oldNick = oldMember.nickname?oldMember.nickname:oldMember.user.username;
+    var newNick = newMember.nickname?newMember.nickname:newMember.user.username;
+    const johnyral = sosi.channels.get(config.channelId);
+    johnyral.send(`Ева сменила ник с ${oldNick} на ${newNick}. Я всё записал. :3`);
+    evaNames.counter++;
+    evaNames.old = oldNick;
+    evaNames.new = newNick;
+
+    evaChanges[Date.now()] = {
+      old : oldNick,
+      new : newNick
+    }
+
+    fs.writeFile('./src/eva_nicnames/last_nickchange.json', JSON.stringify(evaNames), (err) => console.error);
+    fs.writeFile('./src/eva_nicnames/changes.json', JSON.stringify(evaChanges), (err) => console.error);
+
   }
 });
 
@@ -57,6 +91,7 @@ sosi.on('guildMemberRemove', (member) => {
       "end": Date.now(),
     };
     fs.writeFile('./src/last-leave.json', JSON.stringify(newLeave), (err) => console.error);
+    fs.writeFile(`./src/leaves/leave-${new Date()}.json`, JSON.stringify(newLeave), (err) => console.error);
     jorniyal.send('Соунин продержался ' + daysSinceSouninLeft + ' дней и ливнул. Тимми, обновляй счетчик.', {
       files: ['https://media.giphy.com/media/rl0FOxdz7CcxO/giphy.gif']
     });
